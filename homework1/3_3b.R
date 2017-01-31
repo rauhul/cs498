34f1f67e09d4786ea167c2cb1b23a690c0ebca51
rauhulvarma <rauhul.varma@gmail.com>
2017-01-30 18:35:47 -0600

    homework 1 completed

--- homework1/3_3b.R
+++ homework1/3_3b.R
@@ -3,16 +3,15 @@ wdat<-read.csv('processed.cleveland.txt', header=FALSE)
 library(klaR)
 library(caret)
 
-# If Data's final column is greater than 1, count it as one
-non_zero_index <- wdat[, 14] > 0
-wdat[non_zero_index, 14]=1
+# Open the data file
+all_data<-read.csv('processed.cleveland.txt', header = FALSE)
 
 # split the input data into feature vectors and associated label
-x_vec<-wdat[,-c(14)]
-y_vec<-as.factor(wdat[,14])
+x_vec<-wdat[,-c(dim(wdat)[2])]
+y_vec<-as.factor(wdat[,dim(wdat)[2]])
 
 # get indicies to use for training
-wtd<-createDataPartition(y=y_vec, p=.85, list=FALSE)
+wtd<-createDataPartition(y = y_vec, p = .85, list = FALSE)
 
 # extract training feature vectors and labels
 x_vec_train<-x_vec[wtd, ]
@@ -23,11 +22,14 @@ x_vec_test<-x_vec[-wtd, ]
 y_vec_test<-y_vec[-wtd]
 
 # train the model with naive bayes
-model<-train(x_vec_train, y_vec_train, 'nb', trControl=trainControl(method='cv', number=10))
+model<-train(x_vec_train, y_vec_train, 'nb', trControl = trainControl(method='cv', number = 10))
 
 # test the model using the data we did not use for training
-teclasses<-predict(model,newdata=x_vec_test)
+teclasses<-predict(model, newdata = x_vec_test)
+
+# diplsy confusion matrix of results
+confusionMatrix(data = teclasses, y_vec_test)
 
 # print accuracy statistics
-sd(model$resample$Accuracy)
 mean(model$resample$Accuracy)
+sd(model$resample$Accuracy)
