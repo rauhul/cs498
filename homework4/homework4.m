3787ae1861f7dc1a47d542298bfed98c0e2f328f
rauhulvarma <rauhul.varma@gmail.com>
2017-03-02 11:46:59 -0500

    7.10 materials

--- homework4/homework4.m
+++ homework4/homework4.m
@@ -2,13 +2,13 @@
 %% 7.9
 clear; close all; clc
 
-load('/Users/rauhul/Developer/cs498/homework4/brunhild.txt');
+tdfread('brunhild.txt','\t');
 
 figure;
 
 % a)
 subplot(2,1,1);
-scatter(log(brunhild(:, 1)), log(brunhild(:, 2)));
+scatter(log(Hours), log(Sulfate));
 title('Log-Log Plot of Brunhilda Blood Sulfate Levels, with linear regression');
 ylabel('Log( Sulfate )');
 xlabel('Log( Hours )');
@@ -16,32 +16,35 @@ lsline;
 
 % b)
 subplot(2,1,2);
-scatter(brunhild(:, 1), brunhild(:, 2));
+scatter(Hours, Sulfate);
 title('Plot of Brunhilda Blood Sulfate Levels, with linear regression');
 ylabel('Sulfate');
 xlabel('Hours');
 lsline;
 
 % c)
-log_coeff = polyfit(log(brunhild(:, 1)), log(brunhild(:, 2)), 1);
-log_resid = log(brunhild(:, 2)) - ((log(brunhild(:, 1)) .* log_coeff(1)) + log_coeff(2));
-coeff = polyfit(brunhild(:, 1), brunhild(:, 2), 1);
-resid = brunhild(:, 2) - ((brunhild(:, 1) .* coeff(1)) + coeff(2));
+log_coeff = polyfit(log(Hours), log(Sulfate), 1);
+log_resid = log(Sulfate) - ((log(Hours) .* log_coeff(1)) + log_coeff(2));
+coeff = polyfit(Hours, Sulfate, 1);
+resid = Sulfate - ((Hours .* coeff(1)) + coeff(2));
 
 figure;
 subplot(2,1,1);
-scatter(log(brunhild(:, 1)), log_resid);
+scatter(log(Hours), log_resid);
 title('Log-Log Residuals of Brunhilda Blood Sulfate Levels');
 ylabel('Log( Sulfate )');
 xlabel('Log( Hours )');
 lsline;
 
 subplot(2,1,2);
-scatter(brunhild(:, 1), resid);
+scatter(Hours, resid);
 title('Residuals of Brunhilda Blood Sulfate Levels');
 ylabel('Sulfate');
 xlabel('Hours');
 lsline;
 
 %% 7.10
+clear; close all; clc
+
+tdfread('physical.txt','\t');
 
